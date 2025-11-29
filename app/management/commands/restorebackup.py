b50eb3d285045b20a5e38bb5da62d1d3569f930d
evolvestin <steve10.live@gmail.com>
2025-11-29 15:20:52 +0300

    Fixes, upgrades

--- app/management/commands/restorebackup.py
+++ app/management/commands/restorebackup.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+from django.apps import apps
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
@@ -21,6 +22,14 @@ class Command(BaseCommand):
 
         logging.info(f'Using backup file at {backup_file_path}')
 
+        patched_fields = []
+        for model in apps.get_app_config('app').get_models():
+            for field in model._meta.local_fields:
+                if getattr(field, 'auto_now', False) or getattr(field, 'auto_now_add', False):
+                    patched_fields.append((field, field.auto_now, field.auto_now_add))
+                    field.auto_now = False
+                    field.auto_now_add = False
+
         try:
             logging.info('Loading data from JSON backup...')
             call_command('loaddata', backup_file_path)
@@ -32,6 +41,10 @@ class Command(BaseCommand):
                 f'A critical error occurred during backup restoration: {e}', exc_info=True
             )
         finally:
+            for field, auto_now, auto_now_add in patched_fields:
+                field.auto_now = auto_now
+                field.auto_now_add = auto_now_add
+
             if os.path.exists(backup_file_path):
                 os.remove(backup_file_path)
                 logging.info(f'Removed temporary backup file: {backup_file_path}')
